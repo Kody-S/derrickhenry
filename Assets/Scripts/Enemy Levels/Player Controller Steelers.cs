c7c1edd612768cb43ff1cc6be58e7af93744b9a6
Kody-S <76925649+Kody-S@users.noreply.github.com>
2024-02-01 14:23:09 -0500

    tried working on enemy scenes
    tried working on enemy scenes
    didnt work
    fixed derrick henry jersey numbers

--- Assets/Scripts/Enemy Levels/Player Controller Steelers.cs
+++ Assets/Scripts/Enemy Levels/Player Controller Steelers.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.UI;
 
 public class PlayerControllerSteelers : MonoBehaviour
 {
